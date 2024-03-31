436462daf94f43a931c397caff7f5bda4ef69205
Vasili Cassir <vasilicassir@Vasilis-MacBook-Pro.local>
2024-03-31 14:48:16 +0300

    add functionality for sending email in feedback section

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import {Telegraf, Markup} from "telegraf";
 import {message} from "telegraf/filters";
+import nodemailer from 'nodemailer';
 
 const token = '7009879789:AAHhIi1lERScJBUkuZO3eBpRFIitaP2d7h0';
 const webAppUrl = 'https://angular-tg-app-928de.web.app/'
 
 const bot = new Telegraf(token);
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'kasoi129@gmail.com',
+        pass: 'otzi yogx ejtb piwv'
+    }
+});
 
 bot.command('start', (ctx) => {
     ctx.reply(
@@ -20,7 +29,24 @@ bot.command('start', (ctx) => {
 
 bot.on(message('web_app_data'), async (ctx) => {
     const data = ctx.webAppData.data.json();
-    ctx.reply(`Ваше сообщение: ${data?.feedback}` ?? 'empty message');
-})
+    const feedbackMessage = data?.feedback || 'empty message';
+
+    const mailOptions = {
+        from: 'kasoi129@gmail.com',
+        to: 'k.s.s94@mail.ru',
+        subject: 'Feedback',
+        text: feedbackMessage
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+    ctx.reply(`Ваше сообщение: ${feedbackMessage}`);
+});
 
 bot.launch();
